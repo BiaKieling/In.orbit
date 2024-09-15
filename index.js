a5e0b0102af991d8fcb181051d7cde0d7e1838af
Bianca <Biancakieling03@gmail.com>
2024-09-15 20:23:33 -0300

    fundamentos 005

---
+++ index.js
@@ -0,0 +1,19 @@
+const start = () => {
+    
+    while(true){
+        let (opcao) = "sair"
+        switch (opcao) {
+            case "Cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "Listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                return
+        }
+    }
+
+}
+
+start ()
